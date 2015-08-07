878ba70ebf803cdb7e681b6ea98dcc88072ecd44
Sam Urmy <samuel.urmy@stonybrook.edu>
2015-08-07 17:24:41 -0400

    start comparing to calise results...no luck so far

--- sdwba/tests/testthat/test_accuracy.R
+++ sdwba/tests/testthat/test_accuracy.R
@@ -2,7 +2,7 @@ context("Accuracy")
 
 sound.speed <- 1456
 
-generic.krill.m <- generic.krill.Conti2006
+generic.krill.m <- generic.krill.McGeehee1998
 generic.krill.m[c("x", "y", "z", "a")] <- generic.krill.m[c("x", "y", "z", "a")] * 1e-3
 krill <- with(generic.krill.m, Scatterer(x, y, z, a, g, h))
 
@@ -10,9 +10,35 @@ krill <- with(generic.krill.m, Scatterer(x, y, z, a, g, h))
 freqs <- c(38, 120, 200, 333) * 1e3
 
 for (f in freqs) {
-  k <- c(0, 0, f) * 2 * pi / sound.speed
+  k <- c(0, 0, -f) * 2 * pi / sound.speed
   print(paste(f/1000, target.strength(krill, k)))
 }
+
+freq.spec <- frequency.spectrum(krill, 12e3, 400e3, sound.speed, nfreq=120)
+plot(freq.spec$freq, freq.spec$TS, ty='l')
+
+angle.spec <- tilt.spectrum(krill, -90, 270, 120e3, sound.speed, nangle=180)
+angle.spec.c <- tilt.spectrum(krill, -90, 270, 120e3, sound.speed, nangle=180,
+                            method="continuous")
+plot(angle.spec$angle, angle.spec$TS, ty='l')
+lines(angle.spec.c$angle, angle.spec.c$TS, col='blue')
+max(angle.spec$TS)
+
+
+
+angles <- -90:270
+sigma <- rep(0, length(angles))
+k <- c(0, 0, -120e3) * 2 * pi / sound.speed
+phase.sd <- sqrt(2) / 2
+
+for (i in 1:length(angles)) {
+  print(i)
+  sigma[i] <- backscatter.xsection.ensemble(rotate(krill, angles[i]), k, phase.sd)$mean
+}
+TS <- 10 * log10(sigma)
+plot(angles, TS, ty='l')
+
+
 
 
 ## Fluid-sphere
